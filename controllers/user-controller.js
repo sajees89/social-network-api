da0ec0b4d1ab957625df7ffcf794d502a4837d7f
Sajees XPS <sajees.jeya@gmail.com>
2022-03-27 21:30:58 -0400

    routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -50,13 +50,13 @@ const userController = {
         });
       },
       // POST a user
-      creeateUser({ body }, res) {
+      createUser({ body }, res) {
         User.create(body)
           .then((dbUserData) => res.json(dbUserData))
           .catch((err) => res.status(400).json(err));
       },
       // PUT update user
-      updateUseer({ params, body }, res) {
+      updateUser({ params, body }, res) {
         User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
           .then((dbUserData) => {
             if (!dbUserData) {
